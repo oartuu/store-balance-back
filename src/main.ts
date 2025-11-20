6a9ab3ef68f4f420dd9e0727cdf4d27d01eb7adf
Arthur <artu.donascimento@gmail.com>
2025-11-20 13:09:59 -0300

    update CORS to all origins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({
+    origin: "*", // permite qualquer domínio (em produção, troque pelo domínio do front)
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // se for enviar cookies ou auth
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on port: ${process.env.PORT}`);
 }
