364c8f76fd9b0b04b7709d93c54fff921710056c
Arthur <artu.donascimento@gmail.com>
2025-11-23 13:52:08 -0300

    update logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -97,8 +97,7 @@ export class AuthController {
   }
 
   @Post('logout')
-  async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
-    const refreshTokenId = req.cookies['refreshTokenId'];
+  async logout(@Req() req: Request,@Body('refreshTokenId') refreshTokenId: string, @Res({ passthrough: true }) res: Response) {
 
     // Chama serviço para deletar refresh token no DB
     await this.authService.logout(refreshTokenId);
